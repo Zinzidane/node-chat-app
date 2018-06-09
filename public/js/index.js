6d084383cae7ed924269bd23f50c910f5f2d8f0e
Zinzidane <ya-gans@mail.ru>
2018-06-09 12:54:38 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Zidane',
-    text: 'Hey. This is Zizou.'
-  });
 });
 
 socket.on('disconnect', function () {
